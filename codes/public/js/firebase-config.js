adf85d18feaf309b04d48c325daae27fd9221ef2
Samineni Bhavya Sri <2006bhav@gmail.com>
2026-06-21 17:30:23 +0530

    Delete codes/public/js/firebase-config.js

--- codes/public/js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD_xYJ58dRSX8TbsQ20N5YFCag0WK4Za0g",
-  authDomain: "automated-banking-system-c7df4.firebaseapp.com",
-  projectId: "automated-banking-system-c7df4",
-  storageBucket: "automated-banking-system-c7df4.firebasestorage.app",
-  messagingSenderId: "41906911986",
-  appId: "1:41906911986:web:5a98d7695deff8deeaee6b",
-  measurementId: "G-XNSJ3MC5WW"
-};
